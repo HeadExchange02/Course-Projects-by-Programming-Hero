324bbf7e4adb9eb4fa80e982b12a9fe0bfb9be8f
AzmirAntor <antorazmir7@gmail.com>
2026-01-30 20:43:37 +0600

    Meaningful Message

---
+++ Milestone 03/Module 15 Fundamental Concepts of Loops/forSum.js
@@ -0,0 +1,10 @@
+let sum = 0;
+
+for (i = 50; i <= 500; i++){
+
+    sum = sum + i;
+    console.log(i);
+
+}
+
+console.log("Sum of total num: ", sum);
--- Milestone 03/Module 15 Fundamental Concepts of Loops/loop.js
+++ Milestone 03/Module 15 Fundamental Concepts of Loops/loop.js
@@ -1,8 +1,5 @@
-const numbers = [21, 98, 30, 48, 69, 70];
-
-console.log(numbers);
 
 // for loop 
-for(const num of numbers){
-    console.log(num);
+for (let i = 10; i <= 1000; i = i + 10){
+    console.log(i); 
 }
