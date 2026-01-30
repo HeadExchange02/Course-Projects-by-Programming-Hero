fbba3896c16ff8e0c2f8acf3bf0d0c62b8323ca7
AzmirAntor <antorazmir7@gmail.com>
2026-01-30 23:03:49 +0600

    Meaningful Message

---
+++ Milestone 03/Module 16 Fundamentals of String & Objects/case.js
@@ -0,0 +1,13 @@
+const scl = "KGHS";
+
+const subject = "CSE";
+
+console.log(scl);
+console.log(subject);
+
+if(subject === "CSE"){
+    console.log("Talented");
+}
+else{
+    console.log("Not");
+}
---
+++ Milestone 03/Module 16 Fundamentals of String & Objects/string.js
@@ -0,0 +1,15 @@
+const num = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+
+const country = "Bangladesh"
+const capital = "Dhaka"
+const division = "Khilgaon"
+let population = 235600;
+
+console.log(country, capital, division, population);
+console.log(num);
+num[0] = 99;
+console.log(num);
+
+console.log(typeof country, typeof capital, typeof division, typeof population, typeof num);
+
+console.log( country.length, capital.length, division.length, population.length, num.length);
