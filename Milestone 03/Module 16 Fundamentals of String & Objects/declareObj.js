3bfab23e752773dabcd2fe02bf192b0a54a4b0aa
AzmirAntor <antorazmir7@gmail.com>
2026-02-02 18:00:30 +0600

    Completed

---
+++ Milestone 03/Module 16 Fundamentals of String & Objects/declareObj.js
@@ -0,0 +1,18 @@
+const mobile = {
+    brand : "Samsung",
+    model : "S26 Ultra",
+    price : "340000",
+    color : "Charcoil",
+    camera : {
+        quality : "1200mp",
+        zoom : "100x",
+        resulation : "12k",
+    },
+    isNew : true,
+}
+
+const tab = new Object();
+console.log(tab);
+
+const tablet = Object.create({});
+console.log(tablet);
