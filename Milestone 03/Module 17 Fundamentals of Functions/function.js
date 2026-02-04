9437cd17e3b9b9710b6ae5151f70c7f8931563c7
AzmirAntor <antorazmir7@gmail.com>
2026-02-04 23:41:36 +0600

    Completed

---
+++ Milestone 03/Module 17 Fundamentals of Functions/function.js
@@ -0,0 +1,22 @@
+const roll = 90;
+
+
+// Declare function
+function fanOffKor(){
+    console.log("bosha theke uth");
+    console.log("off kor");
+    console.log("on kor");
+    console.log("speed bara");
+}
+
+// Call the function
+fanOffKor();
+
+
+// Declare function
+function fullName(){
+    console.log("Azmir Antor");
+}
+
+// Call the function
+fullName();
---
+++ Milestone 03/Module 17 Fundamentals of Functions/parameter.js
@@ -0,0 +1,42 @@
+// Declare function
+function squreKor(number){
+    console.log(number);
+
+    const squre = number * number;
+    console.log(squre);
+}
+
+// Call the function with parameter's diffrent value.
+squreKor(2);
+console.log("__________");
+squreKor(4);
+squreKor(6);
+
+
+
+console.log("__________");
+
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+
+add(5, 10);
+add(15, 10);
+add(25, 10);
+
+
+
+console.log("__________");
+
+
+function addAll(a, b, c, d, e){
+    const Total = a + b + c + d + e;
+    console.log(Total);
+}
+
+addAll(1, 2, 4, 6);
+addAll(1, 2, 4, 6, 8);
+addAll(1, 2, 4, 6, 8, 10);
